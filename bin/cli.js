bcdb79519e3eadb138ca8cc851d759dc07051fa0
Christofer Bodin <christofer@christoferbodin.se>
2019-03-15 11:11:30 +0100

    Use commander instead of argparse for cli arguments.

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@
   const fs = require('fs');
   const path = require('path');
   const webpack = require('webpack');
-  const { ArgumentParser } = require('argparse');
+  const program = require('commander');
   const resolveApp = (...paths) => path.resolve(process.cwd(), ...paths);
 
   // Load package config
@@ -12,69 +12,47 @@
   const packageInfo = require(packageConfig);
 
   // Configure arguments
-  const parser = new ArgumentParser({
-    version: packageInfo.version,
-    description: packageInfo.description,
-  });
-  parser.addArgument(['-d', '--development'], {
-    help: 'Run in development mode with watcher.',
-    action: 'storeTrue',
-  });
-  parser.addArgument(['--input'], {
-    help: 'Define input folder, default "src/".',
-    defaultValue: 'src/',
-  });
-  parser.addArgument(['--output'], {
-    help: 'Define output folder, default "dist/".',
-    defaultValue: 'dist/',
-  });
-  parser.addArgument(['--entry'], {
-    help: 'Define entry point, default "src/index.js".',
-    defaultValue: 'src/index.js',
-  });
-  parser.addArgument(['--template'], {
-    help: 'Define HTML template file, default "src/index.html".',
-  });
-  parser.addArgument(['--js'], {
-    help: 'Define output JS filename, default "bundle.js".',
-    defaultValue: 'bundle.js',
-  });
-  parser.addArgument(['--css'], {
-    help: 'Define output CSS filename, default "style.css".',
-    defaultValue: 'style.css',
-  });
-  parser.addArgument(['--html'], {
-    help: 'Define output HTML filename, default "index.html".',
-    defaultValue: 'index.html',
-  });
-  parser.addArgument(['--modules'], {
-    help: 'Enable babel for npm modules, comma separated.',
-    defaultValue: '',
-  });
-
-  // Check arguments
-  const args = parser.parseArgs();
-
-  // Ensure absolute paths for input, output and entry.
-  args.input = resolveApp(args.input);
-  args.output = resolveApp(args.output);
-  args.entry = resolveApp(args.entry);
+  program.description(packageInfo.description)
+    .option('--input <path>', 'set input folder path', 'src/')
+    .option('--output <path>', 'set output folder path', 'dist/')
+    .option('--entry <file>', 'set entry file path', 'src/index.js')
+    .option('--template <file>', 'set HTML template file (default: "src/index.html")')
+    .option('--js <filename>', 'set output JS filename', 'bundle.js')
+    .option('--css <filename>', 'set output css filename', 'style.css')
+    .option('--html <filename>', 'set ouput HTML filename', 'index.html')
+    .option('--modules <modules>', 'enable Babel for npm modules, comma separated')
+    .option('-d, --development', 'run in development mode with watcher.')
+    .version(packageInfo.version, '-v, --version')
+    .parse(process.argv);
 
   // Add default template file if none was specified.
   const defaultTemplate = resolveApp('src/index.html');
-  if (!args.template && fs.existsSync(defaultTemplate)) {
-    args.template = defaultTemplate;
-  } else if (args.template) {
-    args.template = resolveApp(args.template);
-  } else {
-    args.template = null;
+  if (!program.template && fs.existsSync(defaultTemplate)) {
+    program.template = defaultTemplate;
   }
 
-  // Create an array of modules and resolve to node_modules
-  args.modules = args.modules.split(',')
-    .map(module => module.trim())
-    .filter(Boolean)
-    .map(module => resolveApp(`node_modules/${module}`));
+  // Split and resolve modules
+  if (program.modules) {
+    program.modules = program.modules.split(',')
+      .map(module => module.trim())
+      .filter(Boolean)
+      .map(module => resolveApp('node_modules', module));
+  } else {
+    program.modules = [];
+  }
+
+  // Prepare arguments
+  const args = {
+    input: resolveApp(program.input),
+    output: resolveApp(program.output),
+    entry: resolveApp(program.entry),
+    template: program.template ? resolveApp(program.template) : null,
+    js: program.js,
+    css: program.css,
+    html: program.html,
+    modules: program.modules,
+    development: !!program.development,
+  };
 
   // Load webpack config
   const configPath = path.resolve(__dirname, '../webpack.config.js');
